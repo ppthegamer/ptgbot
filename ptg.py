1901dc84109daa04ac042471ed4168c566a12a8a
ppthegamer <ppthegamerr@gmail.com>
2020-10-27 09:08:11 +0530

    s

--- ptg.py
+++ ptg.py
@@ -2,10 +2,10 @@ import discord
 import os
 from discord.ext import commands
 
-#client = discord.Client()
+client = discord.Client()
 bot_log = 756116165218009118
 
-client = commands.Bot(command_prefix="$")
+botComm = commands.Bot(command_prefix="$")
 @client.event
 async def on_ready():
 		print('We have logged in as {0.user}'.format(client))
@@ -34,9 +34,8 @@ async def on_message_delete(message):
 		chann = message.guild.get_channel(bot_log)
 		await chann.send(embed=embed)
 
-@client.command()
+@botComm.command()
 async def ping(ctx):
 		await ctx.send("pong")
-		print("Hello")
 
 client.run(os.environ['TOKEN'])	

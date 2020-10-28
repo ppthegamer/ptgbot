77f2294fc290f127a5ba13cedb9254b0e6fb2b1f
ppthegamer <ppthegamerr@gmail.com>
2020-10-28 09:31:32 +0530

    command

--- ptg.py
+++ ptg.py
@@ -29,13 +29,14 @@ async def on_member_join(member,ctx):
 				
 
 @client.event
-async def on_member_remove(member):
+async def on_member_remove(member,ctx):
 		for channel in member.guild.channels:
 			if str(channel)=="test":
 				await ctx.channel.send(f"""{member.mention} **Left the Server**""")
 
 @client.event
 async def on_message_delete(message):
+	
 		#messDeleter = message.author
 		embed = discord.Embed(title=f"""**{message.author}**""",color=0xf40000)
 		h = client.get_channel

80fc8423a1379815b45724ffad41135ec645a2a6
ppthegamer <ppthegamerr@gmail.com>
2020-10-28 09:27:32 +0530

    command

--- ptg.py
+++ ptg.py
@@ -1,36 +1,38 @@
-import discord
+#import discord
 import os
 from discord.ext import commands
+from dotenv import load_dotenv
 
-clientt = discord.Client()
+load_dotenv()
+
+
+client = commands.Bot(command_prefix='!')
+#clientt = discord.Client()
 bot_log = 756116165218009118
-
-client = commands.Bot(command_prefix='$',help_command=None)
-
-@client.command()
-@clientt.event()
-async def ping(ctx):
-		await ctx.channel.send('ssfds')
 
 @client.event
 async def on_ready():
 		print('We have logged in as {0.user}'.format(client))
+
+@client.command(name='!')
+async def ping(ctx):
+		await ctx.send('ssfds')
 @client.event
 async def on_message(message):
 		if message.author == client.user:
 			return
 @client.event
-async def on_member_join(member):
+async def on_member_join(member,ctx):
 		for channel in member.guild.channels:
 			if str(channel)=="test":
-				await channel.send(f"""**Welcome to the server** {member.mention}""") 
+				await ctx.channel.send(f"""**Welcome to the server** {member.mention}""") 
 				
 
 @client.event
 async def on_member_remove(member):
 		for channel in member.guild.channels:
 			if str(channel)=="test":
-				await channel.send(f"""{member.mention} **Left the Server**""")
+				await ctx.channel.send(f"""{member.mention} **Left the Server**""")
 
 @client.event
 async def on_message_delete(message):
@@ -39,7 +41,7 @@ async def on_message_delete(message):
 		h = client.get_channel
 		embed.add_field(name="Message deleted", value=""+message.content + " **was Deleted** "+f"""**by {message.author}**""", inline=False)
 		chann = message.guild.get_channel(bot_log)
-		await chann.send(embed=embed)
+		await ctx.channel.send(embed=embed)
 
 
 

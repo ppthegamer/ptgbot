6491ddec455364eb9ff03729c27a977a2a9fc080
ppthegamer <63738935+ppthegamer@users.noreply.github.com>
2021-01-07 19:04:06 +0530

    Update ptg.py

--- ptg.py
+++ ptg.py
@@ -25,6 +25,7 @@ async def on_message(message):
 			return
 @client.event
 async def on_member_join(member,ctx):
+    print('Member Joined')
     role = discord.utils.get(member.server.roles, id=796709135344992257)
     await client.add_roles(member, role)
 		

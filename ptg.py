028b234c676ee9477b6a4390d3956cc9ef3f65e1
ppthegamer <63738935+ppthegamer@users.noreply.github.com>
2021-01-07 19:26:24 +0530

    Update ptg.py

--- ptg.py
+++ ptg.py
@@ -19,27 +19,7 @@ async def on_message(message):
 @client.event
 async def on_member_join(member,ctx):
     	print('Member Joined')
-    	role = discord.utils.get(member.server.roles, id='796709135344992257')
+    	role = discord.utils.get(member.server.roles, name="Members")
     	await client.add_roles(member, role)
-		
-				
-
-@client.event
-async def on_member_remove(member,ctx):
-		for channel in member.guild.channels:
-			if str(channel)=="test":
-				await ctx.channel.send(f"""{member.mention} **Left the Server**""")
-
-@client.event
-async def on_message_delete(message,ctx):
-	
-		#messDeleter = message.author
-		embed = discord.Embed(title=f"""**{message.author}**""",color=0xf40000)
-		#h = clientA.get_channel
-		embed.add_field(name="Message deleted", value=""+message.content + " **was Deleted** "+f"""**by {message.author}**""", inline=False)
-		#chann = message.guild.get_channel(bot_log)
-		await ctx.channel.send(embed=embed)
-
-
 
 client.run(token)	
